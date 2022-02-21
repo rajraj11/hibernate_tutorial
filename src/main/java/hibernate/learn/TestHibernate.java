db6a75af4f0ccd3da0614303f09fb0a226f09b78
rajraj11 <fakeidraj1020@gmail.com>
2022-02-22 01:06:11 +0530

    Entity save in database

---
+++ src/main/java/hibernate/learn/TestHibernate.java
@@ -0,0 +1,31 @@
+package hibernate.learn;
+
+import entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class TestHibernate {
+    public static void main(String[] args) {
+        SessionFactory factory=new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        Session session=factory.getCurrentSession();
+        try
+        {
+            Student tempStudent=new Student("Kamal","Raj","kamal@123");
+            session.beginTransaction();
+            session.save(tempStudent);
+            session.getTransaction().commit();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            factory.close();
+        }
+    }
+}
